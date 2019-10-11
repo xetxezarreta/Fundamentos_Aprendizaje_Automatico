27206df2ba5a3710de28fed23ba6729145e2de45
xetxezarreta <xabi1500@gmail.com>
2019-10-11 19:20:24 +0200

    load iris

---
+++ 08_validacion_modelos/ejercicio.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+class CrossValidator():
+    def __init__(self, dataset, k_folds, algorithm):
+        self.dataset = dataset
+        self.k_folds = k_folds
+        self.algorithm = algorithm
+    
+    def getFolds(self):
+        pass
+
+    def crossValidate(self):
+        pass
+
+
+data = datasets.load_iris()
